50af7108c0bd41f83c3e75409de36028dfc6349b
popofffnik <57560494+popofffnik@users.noreply.github.com>
2022-12-23 18:31:33 +0300

    Добавьте файлы проекта.

---
+++ Laba3.2/Laba3.2.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+
+#include <Windows.h>
+
+using namespace std;
+
+
+
+int arr_sum(int* begin, int* end) {
+
+	int sum = 0;
+
+	for (int* p = begin; p < end; p++) {
+
+		sum += *p;
+
+	}
+
+	return sum;
+
+}
+
+
+
+int sum(int a, int b) {
+
+	return a + b;
+
+}
+
+int dif(int a, int b) {
+
+	return a - b;
+
+}
+
+int (*operationType(char sType))(int, int) {
+
+	if (sType == '+') return sum;
+
+	if (sType == '-') return dif;
+
+	else cout << "wrong symbol (+/-)";
+
+}
+
+
+
+int main()
+
+{
+
+	int m[] = { 2,4,8,12 };
+
+	cout << arr_sum(begin(m), end(m));
+
+
+
+	cout << "\n---------------------\n";
+
+
+
+	int (*test)(int, int);
+
+	test = operationType('+');
+
+	cout << test(25, 15) << endl;
+
+	test = operationType('-');
+
+	cout << test(25, 15);
+
+
+
+	cout << "\n---------------------\n";
+
+
+
+	float* pntr = new float(24.6f);
+
+	cout << *pntr << endl;
+
+	delete pntr;
+
+}
